4b58cc8bda5fb95cb896caacde8f0133d1c35659
João Victor Rodrigues de Souza <joao.victor@mag-it.com.br>
2022-05-22 20:06:51 -0300

    Estrutura de ConsoleApplication seguindo padrão do .Net 6 e C# 10.

---
+++ FRMK.ConsoleApp/Executor.cs
@@ -0,0 +1,38 @@
+﻿using FRMK.IBLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FRMK.ConsoleApp
+{
+    public class Executor
+    {
+        private readonly ICalculator _calculator;
+
+        public Executor(ICalculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        public void Execute()
+        {
+            int number;
+            Console.WriteLine("Insira o número para o cálculo:");
+            var value = Console.ReadLine();
+            if (!int.TryParse(value, out number))
+                Console.WriteLine("Valor inválido, insira um número inteiro.");
+
+            var response = _calculator.GetDivisorsAndPrimeDivisors(number);
+
+            if(response != null)
+            {
+                Console.WriteLine("Divisores:" + string.Join(", ", response.Divisors));
+                Console.WriteLine("Divisores Primos:" + string.Join(", ", response.PrimeDivisors));
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
--- FRMK.ConsoleApp/Program.cs
+++ FRMK.ConsoleApp/Program.cs
@@ -1,1 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using FRMK.ConsoleApp;
+using FRMK.IoC;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((_, services) =>
+        services.RegisterServices()
+        .AddScoped<Executor>())
+    .Build();
+
+host.Services.GetService<Executor>()?.Execute();
+
+await host.RunAsync();
